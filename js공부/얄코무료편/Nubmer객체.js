37b91bf805016ec63bc0c0c2d72fe8ceaa935496
Yuseong Noh <shdbtjd8@gmail.com>
2023-01-25 20:57:12 +0900

    add 얄코무료편_folder

---
+++ js공부/얄코무료편/Nubmer객체.js
@@ -0,0 +1,15 @@
+// const num = new Number(123);
+// console.log(typeof num); // data type : object
+// console.log(num.valueOf());
+// console.log(
+//     new Number('-123123'), // 수로 입력된다.
+//     new Number('ABC'), // NaN
+//     new Number(true) // 1로 입력된다.
+// );
+
+
+
+
+
+// Number의 static property
+console.log(Number.EPSILON); // 자바스크립트에서 0에 가장 가까운 수
