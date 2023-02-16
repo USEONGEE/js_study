8fc911cbdc3a8b3ecb8288ec3b4030944652dcc7
Yuseong Noh <shdbtjd8@gmail.com>
2023-02-16 19:05:46 +0900

    clonecoding - toggle

--- clonecoding/src/js/app.js
+++ clonecoding/src/js/app.js
@@ -1,17 +1,22 @@
-const h1 = document.querySelector("div.hello:first-child h1") 
+const h1 = document.querySelector("div.hello:first-child h1")
 
 // function handleTitleClick() {
 //     h1.className = "active";
 // }
 
-function handleTitleClick() { // 이전에 가지고 있었던 class를 전부 없앤다는 단점이 있음
-    const activeClass = "active"
-    if (h1.classList.contains(activeClass)){
-        h1.classList.remove(activeClass)
-    }
-    else {
-        h1.classList.add(activeClass)
-    }
+// function handleTitleClick() { // 이전에 가지고 있었던 class를 전부 없앤다는 단점이 있음
+//     const activeClass = "active"
+//     if (h1.classList.contains(activeClass)){
+//         h1.classList.remove(activeClass)
+//     }
+//     else {
+//         h1.classList.add(activeClass)
+//     }
+// }
+
+function handleTitleClick() {
+    h1.classList.toggle("active")
 }
 
-h1.addEventListener("click",handleTitleClick)  
+
+h1.addEventListener("click", handleTitleClick)  
