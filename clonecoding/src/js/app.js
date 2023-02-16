c534725d6009325f0f2e18ae202312715d5781f2
Yuseong Noh <shdbtjd8@gmail.com>
2023-02-16 19:03:27 +0900

    clonecoding - classList

--- clonecoding/src/js/app.js
+++ clonecoding/src/js/app.js
@@ -6,12 +6,12 @@ const h1 = document.querySelector("div.hello:first-child h1")
 
 function handleTitleClick() { // 이전에 가지고 있었던 class를 전부 없앤다는 단점이 있음
     const activeClass = "active"
-    if (h1.className === activeClass){
-        h1.className = ""
+    if (h1.classList.contains(activeClass)){
+        h1.classList.remove(activeClass)
     }
     else {
-        h1.className = activeClass
+        h1.classList.add(activeClass)
     }
 }
 
-h1.addEventListener("click",handleTitleClick)
+h1.addEventListener("click",handleTitleClick)  
